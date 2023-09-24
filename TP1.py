ebcd54e917c4be88250d382d0a21e3630c0db281
Benoit Faure <145790936+BenoitFaure@users.noreply.github.com>
2023-09-23 22:03:44 -0400

    First Commit + TP1
    Add poetry, empty readme and TP1

---
+++ TP1.py
@@ -0,0 +1,129 @@
+# TP1 - Algorithmes de réduction de dimentionalité
+
+# Imports
+import time
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest, f_regression
+
+# -----------------DATAPROCESSING-----------------
+print("1] DATAPROCESSING")
+
+# a) Load data
+df = pd.read_csv('train.csv')
+print(df.head(2))
+
+
+# b) Missing values
+df.fillna(0, inplace=True) # Replace missing values by 0
+
+# c) Categorical variable
+df_dummies = pd.get_dummies(df) # One hot encoding
+
+# d) Extract features and target values
+X, y = df_dummies.drop(columns=['Sales']), df_dummies[['Sales']]
+y_log = np.log(y['Sales']) #Scale the Sales to make it more uniform
+
+# ------------DIMENSIONALITY REDUCTION------------
+print("2] DIMENSIONALITY REDUCTION")
+
+num_features = 10
+print(f"Reducing to {num_features} features")
+
+# a) PCA - sklearn  ------------------------------
+print("a) PCA - sklearn")
+
+pca_sk_exec_time = time.time()
+
+pca_sk_obj = PCA(n_components=num_features)
+X_pca_sk = pca_sk_obj.fit_transform(X)
+
+pca_sk_exec_time = time.time() - pca_sk_exec_time
+
+print(f"Done in {pca_sk_exec_time} s")
+
+# b) PCA -----------------------------------------
+print("b) PCA")
+
+def my_pca(_X, num_feat=None):
+    X_meaned = _X - np.mean(_X , axis = 0)
+    z = X_meaned.cov()
+    eigen_values , eigen_vectors = np.linalg.eig(z)
+
+    #sort the eigenvalues in descending order
+    sorted_index = np.argsort(eigen_values)[::-1]
+    sorted_eigenvalue = eigen_values[sorted_index]
+
+    #similarly sort the eigenvectors
+    sorted_eigenvectors = eigen_vectors[:,sorted_index]
+
+    if num_feat:
+        sorted_eigenvectors = sorted_eigenvectors[:,:num_feat]
+
+    vp_tr = sorted_eigenvectors.transpose()
+    return vp_tr * _X
+
+pca_exec_time = time.time()
+
+X_pca = my_pca(X, num_feat=num_features)
+
+pca_exec_time = time.time() - pca_exec_time
+
+print(f"Done in {pca_exec_time} s")
+
+# c) SelectKBest ---------------------------------
+print("c) SelectKBest (f_regression)")
+
+skb_exec_time = time.time()
+
+skb_obj = SelectKBest(f_regression, k=num_features)
+X_skb = skb_obj.fit_transform(X, y_log)
+
+skb_exec_time = time.time() - skb_exec_time
+
+print(f"Done in {skb_exec_time} s")
+print(skb_obj.get_feature_names_out(X.columns))
+
+# -------------------COMPARISON-------------------
+# Do clustering for 2d visulaisation
+tsne = TSNE(n_components=2, perplexity=30, random_state=42)
+
+X_tsne_pca_sk = tsne.fit_transform(X_pca_sk)
+X_tsne_pca = tsne.fit_transform(X_pca)
+X_tsne_skb = tsne.fit_transform(X_skb)
+
+#Visualise tsne for PCA sklearn
+
+plt.figure(figsize=(8, 6))
+plt.scatter(X_tsne_pca_sk[:, 0], X_tsne_pca_sk[:, 1], c=y_log, cmap='viridis', marker='o', s=50)
+plt.colorbar(label='Regression Target (log(Sales))')
+plt.title('t-SNE Visualization for PCA sklearn')
+plt.xlabel('Dimension 1')
+plt.ylabel('Dimension 2')
+plt.show()
+
+#Visualise tsne for myPCA
+
+plt.figure(figsize=(8, 6))
+plt.scatter(X_tsne_pca[:, 0], X_tsne_pca[:, 1], c=y_log, cmap='viridis', marker='o', s=50)
+plt.colorbar(label='Regression Target (log(Sales))')
+plt.title('t-SNE Visualization for PCA')
+plt.xlabel('Dimension 1')
+plt.ylabel('Dimension 2')
+plt.show()
+
+#Visualise tsne for SelectKBest
+
+plt.figure(figsize=(8, 6))
+plt.scatter(X_tsne_skb[:, 0], X_tsne_skb[:, 1], c=y_log, cmap='viridis')
+plt.colorbar(label='Regression Target (log(Sales))')
+plt.title('t-SNE Visualization for SelectKBest')
+plt.xlabel('Dimension 1')
+plt.ylabel('Dimension 2')
+plt.show()
